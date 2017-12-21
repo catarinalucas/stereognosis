b57fc5f47d578c0fb1e0564ee4252ca4c814e095
Hugo Vale Pereira <hvpereira@gmail.com>
2017-12-21 15:28:39 +0000

    nav

--- init.js
+++ init.js
@@ -1,29 +1,35 @@
 jQuery(function($) {
-    //quando se clica na da esquerda, a da direita abre
-    $('.purpose-l-click-js').click(function(){
-        if($('.purpose').hasClass('purpose--right-clicked')){
-            $('.purpose').removeClass('purpose--right-clicked');    
-        }
-        $('.purpose').addClass('purpose--left-clicked');
-        
-    });
-    
-    $('.purpose-r-click-js').click(function(){
-        if($('.purpose').hasClass('purpose--left-clicked')){
-            $('.purpose').removeClass('purpose--left-clicked');    
-        }
-        $('.purpose').addClass('purpose--right-clicked');
-    });
-    
-    $('footer').click(function(evt){
-        if(!evt.target.matches(".purpose")){
-            if($('.purpose').hasClass('purpose--left-clicked')){
-                $('.purpose').removeClass('purpose--left-clicked');    
-            }
-            else if($('.purpose').hasClass('purpose--right-clicked')){
-                $('.purpose').removeClass('purpose--right-clicked');    
-            }
-            console.log("oh hai mark");
-        }
-    });
+  //quando se clica na da esquerda, a da direita abre
+  $('.purpose-l-click-js').click(function(){
+    if($('.purpose').hasClass('purpose--right-clicked')){
+      $('.purpose').removeClass('purpose--right-clicked');
+    }
+    $('.purpose').addClass('purpose--left-clicked');
+
+  });
+
+  $('.purpose-r-click-js').click(function(){
+    if($('.purpose').hasClass('purpose--left-clicked')){
+      $('.purpose').removeClass('purpose--left-clicked');
+    }
+    $('.purpose').addClass('purpose--right-clicked');
+  });
+
+  $('nav').click(function(){
+    $(this).toggleClass('down');
+  }
+
+);
+
+$('footer').click(function(evt){
+  if(!evt.target.matches(".purpose")){
+    if($('.purpose').hasClass('purpose--left-clicked')){
+      $('.purpose').removeClass('purpose--left-clicked');
+    }
+    else if($('.purpose').hasClass('purpose--right-clicked')){
+      $('.purpose').removeClass('purpose--right-clicked');
+    }
+    console.log("oh hai mark");
+  }
+});
 });
