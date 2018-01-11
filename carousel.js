5d34dbf926d5630a931ab10667fd5e7aa1ff13ae
Catarina Lucas <catarinalucasloureiro@gmail.com>
2018-01-11 23:19:41 +0000

    New_Carousel
    Falta resolver o scroll speed

---
+++ carousel.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  
+    var owl = $("#owl-demo");
+    
+$('#owl-demo').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    startPosition:7,
+    items: 1,
+    itemsDesktop: false,//1400:screen size, 3: number if items in the slide
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsMobile: false
+})
+   
+   
+        
+    owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+})
+        
+    
+    
+
+ });
+
+
+
